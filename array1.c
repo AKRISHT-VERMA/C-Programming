6cb1e002844bee8e89eb60b5176b28c3216124e5
AKRISHT VERMA <akrishtverma05@gmail.com>
2026-01-30 15:36:42 +0000

    performed loop operation on array for storing and printing elemets

---
+++ array1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() {
+    int arr[10];
+    printf("Enter 10 Numbers: \n");
+    for(int i = 0 ; i < 10 ; i++) {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("You Entered: \n");
+    for(int i = 0; i < 10; i++) {
+        printf("%d",arr[i]);
+    }
+
+    return 0;
+}
