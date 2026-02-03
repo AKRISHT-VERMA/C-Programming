fa9eb29b335a3e801757b1b60c9be5b995a5615c
AKRISHT VERMA <akrishtverma05@gmail.com>
2026-02-03 11:50:27 +0000

    Add implementation of logical OR operator program

---
+++ Logical_Operators/or_OP.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if(a > 0 || b > 0) {
+        printf("At least one number is positive");
+    } else {
+        printf("Both numbers are negative or zero");
+    }
+
+    return 0;
+}
