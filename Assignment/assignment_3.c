2dded64dc81127ba511e367eecf13f0816f3d617
AKRISHT VERMA <akrishtverma05@gmail.com>
2026-02-05 15:32:56 +0000

    Implement multiplication table functionality in assignment_3 program

--- Assignment/assignment_3.c
+++ Assignment/assignment_3.c
@@ -6,5 +6,11 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    
+    printf("\nMultiplication Table of %d:\n", num);
+
+    for(i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
 }
