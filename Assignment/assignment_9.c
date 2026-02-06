ddea453c14afa2bbd99bb35bc7d576bf7d44d38a
AKRISHT VERMA <akrishtverma05@gmail.com>
2026-02-06 06:50:48 +0000

    Build the logic for the armstrong number to execute

---
+++ Assignment/assignment_9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num, originalNum, remainder, result = 0;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    originalNum = num;
+
+    while (originalNum != 0) {
+        remainder = originalNum % 10;
+        result += remainder * remainder * remainder;
+        originalNum /= 10;
+    }
+}
