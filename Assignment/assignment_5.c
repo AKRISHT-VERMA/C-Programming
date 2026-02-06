a77f8360f098157e85ee24366fcbf228d1b059bf
AKRISHT VERMA <akrishtverma05@gmail.com>
2026-02-06 06:41:54 +0000

    Add implementation for percentage calculation and grading system in assignment_5

---
+++ Assignment/assignment_5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // To calculate the percentage of marks obtained by a student in 5 subjects and student 
+    // is considered pass if he scores 40 marks or more in each subject.
+    // to assign grades to students based on their percentage as follows:
+    // aggregate percentage >= 75% - Distinction
+    // aggregate percentage >= 60% and < 75% - First division
+    // aggregate percentage >= 50% and < 60% - Second division
+    // aggregate percentage >= 40% and < 50% - Third division
+    int marks[5];
+    int total = 0;
+    float percentage;
+
+    printf("Enter marks obtained in 5 subjects:\n");
+    for(int i = 0; i < 5; i++) {
+        printf("Subject %d: ", i + 1);
+        scanf("%d", &marks[i]);
+        total += marks[i];
+}
