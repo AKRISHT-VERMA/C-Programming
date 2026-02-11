9a050ad4187181327bcaf18a1ecdcb349547ac8e
AKRISHT VERMA <akrishtverma05@gmail.com>
2026-02-11 18:52:03 +0000

    implemented the loop structures

--- Assignment/assignment_11.c
+++ Assignment/assignment_11.c
@@ -17,5 +17,16 @@ int main()
     
     int cube = num * num * num;
 
-    
+    if (num < 0)
+    {
+        printf("Factorial is not defined for negative numbers.\n");
+    }
+    else
+    {
+        for (i = 1; i <= num; i++)
+        {
+            factorial = factorial * i;
+        }
+    }
+
 }
