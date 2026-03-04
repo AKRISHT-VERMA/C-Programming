d006e4dabb55657d3a4355b536e0280ff25210e4
AKRISHT VERMA <akrishtverma05@gmail.com>
2026-03-04 19:33:01 +0000

    add input prompts for string comparison

--- strings/string_cmp.c
+++ strings/string_cmp.c
@@ -6,5 +6,11 @@ int main() {
     char str1[100];
     char str2[100];
 
+    printf("Enter first string: ");
+    fgets(str1, sizeof(str1), stdin);
+
+    printf("Enter second string: ");
+    fgets(str2, sizeof(str2), stdin);
+
     
 }
