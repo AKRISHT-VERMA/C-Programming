92ab87ba555e310e9a816fe89ce4ea9ae808817f
AKRISHT VERMA <akrishtverma05@gmail.com>
2026-04-09 06:19:44 +0000

    add: implement file copy program using file handling in C

---
+++ Assignment/assignment_21.c
@@ -0,0 +1,46 @@
+// program to copy contents of one file to another file using file handling in C
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *sourceFile, *destinationFile;
+    char sourceFileName[100], destinationFileName[100];
+    char ch;
+
+    
+    printf("Enter the name of the source file: ");
+    scanf("%s", sourceFileName);
+
+    
+    sourceFile = fopen(sourceFileName, "r");
+    if (sourceFile == NULL) {
+        perror("Error opening source file");
+        return EXIT_FAILURE;
+    }
+
+    
+    printf("Enter the name of the destination file: ");
+    scanf("%s", destinationFileName);
+
+    
+    destinationFile = fopen(destinationFileName, "w");
+    if (destinationFile == NULL) {
+        perror("Error opening destination file");
+        fclose(sourceFile);
+        return EXIT_FAILURE;
+    }
+
+    
+    while ((ch = fgetc(sourceFile)) != EOF) {
+        fputc(ch, destinationFile);
+    }
+
+    printf("File copied successfully.\n");
+
+    
+    fclose(sourceFile);
+    fclose(destinationFile);
+
+    return EXIT_SUCCESS;
+
+}
